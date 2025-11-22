f7e35a8878525e48b0f1dc11c35eef53ab86a20a
Eduardo Soleno Moriz Liborio <eduardo.liborio@VENTTOS.local>
2025-11-22 17:34:51 -0400

    add

--- ping.py
+++ ping.py
@@ -1,12 +1,12 @@
 import time
 import requests
 
-# #URL = https://sistema-meta-tool-venttos.onrender.com
+URL = "https://sistema-meta-tool-venttos.onrender.com"
 
-# while True:
-#     try:
-#         r = requests.get(URL)
-#         print("Ping OK:", r.status_code)
-#     except Exception as e:
-#         print("Erro:", e)
-#     time.sleep(300)  # 5 minutos
+while True:
+    try:
+        r = requests.get(URL)
+        print("Ping OK:", r.status_code)
+    except Exception as e:
+        print("Erro:", e)
+    time.sleep(300)  # 5 minutos
